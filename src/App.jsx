d7cdbdc7c48c527a321248c5cf67bea7e4b58a96
LAPTOP-T21LK98P\KHS <https://github.com/Hosae-Coding>
2021-10-04 21:56:41 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { makeStyles } from '@material-ui/core';
+import Navbar from './components/Navbar';
+
+const useStyles = makeStyles((theme) => ({}));
+
+const App = () => {
+   const classes = useStyles();
+   return (
+      <div>
+         <Navbar />
+      </div>
+   );
+};
+
+export default App;
